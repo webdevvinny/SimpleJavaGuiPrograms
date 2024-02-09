8d147573e996fc9dfd0d6c598fb1e8093badd165
Web Dev Vinny <whiteangel@duck.com>
2024-02-09 09:05:05 +0300

    Java Gui Programs

--- src/GUIToCaptureUserInput.java
+++ src/GUIToCaptureUserInput.java
@@ -1,2 +1,37 @@
+
+import javax.swing.*;
+
 public class GUIToCaptureUserInput {
+
+    GUIToCaptureUserInput{
+            JFrame userInputForm = new JFrame("Capture User Input!!");
+            JLabel nameInputField = new JLabel("Enter your name: ");
+            JLabel ageInputField = new JLabel("Enter your age: ");
+            JLabel genderInputField = new JLabel("Enter your Gender: ");
+            JLabel countryOfOriginInputField = new JLabel("Enter your Country of Origin: ");
+
+            nameInputField.setBounds(0,0,100,100);
+            ageInputField.setBounds(0,20,100,100);
+            genderInputField.setBounds(0,40,100,100);
+            countryOfOriginInputField.setBounds(0,80,100,100);
+
+            userInputForm.add(nameInputField);
+            userInputForm.add(ageInputField);
+            userInputForm.add(genderInputField);
+            userInputForm.add(countryOfOriginInputField);
+
+            userInputForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            userInputForm.setResizable(true);
+            userInputForm.setLayout(null);
+            userInputForm.setSize(400, 300);
+            userInputForm.setVisible(true);
+
+
+    }
+
+    public static void main(String [] args){
+            new GUIToCaptureUserInput();
+
+
+    }
 }
